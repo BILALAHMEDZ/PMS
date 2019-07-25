3208819289e9410033f58ac1a5faa01d62385725
BILALZAHEER <bitf15m511@pucit.edu.pk>
2019-07-25 18:44:05 +0500

    Devise:UserTable:Remove colmuns and change datatype

---
+++ db/migrate/20190725133843_remove_columns_from_user.rb
@@ -0,0 +1,7 @@
+class RemoveColumnsFromUser < ActiveRecord::Migration[5.2]
+  def change
+    remove_column :users, :age, :integer
+    remove_column :users, :phone, :string
+    remove_column :users, :role, :boolean
+  end
+end
