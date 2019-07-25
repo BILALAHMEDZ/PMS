f81483947790ffc1948dd225ba97ab55d4f17f62
BILALZAHEER <bitf15m511@pucit.edu.pk>
2019-07-25 16:12:37 +0500

    User:Model:Added Validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,6 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  validates :name, presence: true
+  validates :age,:phone , numericality: { only_integer: true }
 end
