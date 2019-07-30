e50b582260538f9a09082b483539b4f6acabe03e
BILALAHMEDZ <bilal.ahmed@square63.org>
2019-07-30 15:17:38 +0500

    Clients: Model validations

--- app/models/client.rb
+++ app/models/client.rb
@@ -3,6 +3,8 @@
 class Client < ApplicationRecord
   validates :name, presence: true
   validates :phone, presence: true
+  validates :phone, length: { is: 11 }
+  validates :email, uniqueness: true
   paginates_per 5
 
   def self.search(search)
