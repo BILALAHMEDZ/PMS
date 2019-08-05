fb7e10a76105411140b85e576e127d25a5ba31ad
BILALAHMEDZ <bilal.ahmed@square63.org>
2019-08-05 14:12:15 +0500

    Client: Employee: Views Updated

--- app/controllers/employee/clients_controller.rb
+++ app/controllers/employee/clients_controller.rb
@@ -4,4 +4,9 @@ class Employee::ClientsController < ApplicationController
   def index
     @clients = Client.search(params[:search]).order(:id).page(params[:page])
   end
+
+  def show
+    @client = Client.find_by_id(params[:id])
+    render file: 'public/404.html', status: :not_found, layout: false unless @client
+  end
 end
