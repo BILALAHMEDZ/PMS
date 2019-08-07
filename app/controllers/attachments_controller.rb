23a07ccb74b1c2e2fb1ab165e540515ff0e51ca0
BILALAHMEDZ <bilal.ahmed@square63.org>
2019-08-07 13:53:31 +0500

    Project: Attachments created

--- app/controllers/attachments_controller.rb
+++ app/controllers/attachments_controller.rb
@@ -1,18 +1,14 @@
-# frozen_string_literal: true
-
 class AttachmentsController < ApplicationController
   def create
+    byebug
     @attachment = Attachment.create(attachment_params)
     redirect_to request.referrer, notice: 'Your attachment was successfully added' if @attachment.save
   end
-
   def destroy
     @attachment = Attachment.find(params[:id])
     redirect_to request.referrer, notice: 'Attachment was successfully remove.' if @attachment.destroy
   end
-
   private
-
   def attachment_params
     params.require(:attachment).permit(:attachment, :project_id)
   end
